7af9b481854575043bdfecde9bbae80e29fbeb03
juamqi <juampikass@gmail.com>
2025-11-17 02:41:12 -0300

    D

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Fail fast
+set -e
+
+# Migraciones
+python manage.py migrate --noinput
+
+# Archivos estáticos
+python manage.py collectstatic --noinput
+
+# Arrancar gunicorn en el puerto que exige Railpack ($PORT)
+gunicorn ambos_backend.wsgi:application --bind 0.0.0.0:$PORT
