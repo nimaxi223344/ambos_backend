7ca4f863eea926480c0f4489bbaece1f81714a9c
juamqi <juampikass@gmail.com>
2025-11-17 04:15:51 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # Arrancar gunicorn en el puerto que exige Railpack ($PORT)
-gunicorn ambos_backend.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn ambos_norte.wsgi:application --bind 0.0.0.0:$PORT
